8c23c97bd09594a97f9a4ab17b5938b17d85bd63
Kureev Alexey <kureev-mail@ya.ru>
2015-08-07 03:12:27 +0200

    Correct parameter name

--- makeWatcher.js
+++ makeWatcher.js
@@ -5,8 +5,8 @@ const Logdown = require('logdown');
 const moment = require('moment');
 const logger = new Logdown({ prefix: '[BDS:SYSTEM]', });
 
-module.exports = function makeWatcher(bundle) {
-  return chokidar.watch(bundle)
+module.exports = function makeWatcher(path) {
+  return chokidar.watch(path)
     .on('error', function processError(err) {
       logger.error('Oops, an error has been occured: ' + err);
     });
