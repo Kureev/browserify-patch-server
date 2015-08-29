80515ab812ec853f31708df8b914d3dd6e359274
Kureev Alexey <kureev-mail@ya.ru>
2015-08-29 22:09:38 +0200

    Check sources length before apply patch

--- makePatch.js
+++ makePatch.js
@@ -4,9 +4,16 @@ module.exports = function scopeOriginal(sources) {
   return function calculateDiff(file, changedContent) {
     if (!changedContent.length) return false;
 
-    const originalContent = sources.filter(function filterSources(source) {
+    const fileMatches = sources.filter(function filterSources(source) {
       return source.file === file;
-    })[0].content;
+    });
+
+    if (!fileMatches.length) {
+      throw Error('Directory pattern you specified to watch doesn\'t contain ' +
+      'any files. Please, check the pattern you specified and try again.');
+    }
+
+    const originalContent = fileMatches[0].content;
 
     return jsdiff.createPatch(Date.now(), originalContent, changedContent);
   };
